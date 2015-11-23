816b9c623bf6206cfd7e06ea42a8089302a0e34b
DCowboy <demoncowboy@hotmail.com>
2015-11-22 20:07:28 -0500

    attempting a performance fix by limiting non-visible things

--- Star_Game/shared/mini_info.gd
+++ Star_Game/shared/mini_info.gd
@@ -6,12 +6,19 @@ var lbl_name
 var lbl_health
 var offset
 var margin = 5
+var visibility
 
 func _ready():
+	visibility = get_node("VisibilityNotifier2D")
 	lbl_health = get_node("lbl_health")
 	lbl_name = get_node("lbl_name")
 	var parent_rect = get_parent().get_item_rect()
+	print(parent_rect)
 	offset = Vector2(0, -parent_rect.size.height / 2)
+	visibility.set_rect(Rect2(Vector2(0, 0), Vector2(parent_rect.size * 1.25)))
+	if not visibility.is_on_screen():
+		self.hide()
+		get_parent().hide()
 	set_process(true)
 
 func _process(delta):
@@ -22,3 +29,19 @@ func _process(delta):
 		lbl_health.set_text(str(round(health)) + ' / ' + str(round(max_health)))
 	lbl_health.set_pos(Vector2(lbl_health.get_pos().x,  -lbl_health.get_size().height - margin) + offset)
 	lbl_name.set_pos(Vector2(lbl_name.get_pos().x, lbl_health.get_pos().y -lbl_name.get_size().height - margin))
+	
+	
+	
+	
+	
+
+func _on_VisibilityNotifier2D_enter_screen():
+	self.show()
+	get_parent().show()
+	pass # replace with function body
+
+
+func _on_VisibilityNotifier2D_exit_screen():
+	self.hide()
+	get_parent().hide()
+	pass # replace with function body

ae04b1440fb3e98c2f13ff6defd6da3cc63128a3
DCowboy <demoncowboy@hotmail.com>
2015-12-17 15:22:54 -0500

    maybe I forgot something last night?

---
+++ Star_Game/selection_screen.gd
@@ -0,0 +1,47 @@
+
+extends Panel
+
+var client
+
+func _ready():
+	client = preload('res://client.scn')
+	pass
+
+
+func _on_terran_corvette_mouse_enter():
+	get_node("explanation").set_text('Terran Corvette: Currently does the same damn thing as every ship you see here')
+	pass # replace with function body
+
+
+func _on_terran_corvette_mouse_exit():
+	get_node("explanation").set_text('')
+	pass # replace with function body
+
+
+func _on_terran_warship_mouse_enter():
+	get_node("explanation").set_text('Terran Warship: Currently does the same damn thing as every ship you see here')
+	pass # replace with function body
+	
+	
+func _on_terran_warship_mouse_exit():
+	get_node("explanation").set_text('Terran Warship: Currently does the same damn thing as every ship you see here')
+	pass # replace with function body
+	
+	
+func _on_terran_corvette_pressed():
+	get_node("/root/globals").player['ship'] = get_node("/root/globals").ships.terran_corvette.scene
+	get_node("/root/globals").player['status'] = get_node("/root/globals").ships.terran_corvette.status
+	get_node("choice").set_text('Current Choice: Terran Corvette')
+	pass # replace with function body
+
+
+func _on_terran_warship_pressed():
+	get_node("/root/globals").player['ship'] = get_node("/root/globals").ships.terran_warship.scene
+	get_node("/root/globals").player['status'] = get_node("/root/globals").ships.terran_warship.status
+	get_node("choice").set_text('Current Choice: Terran Warship')
+	pass # replace with function body
+
+
+func _on_confirm_pressed():
+	call_deferred('replace_by', client.instance())
+	pass # replace with function body
